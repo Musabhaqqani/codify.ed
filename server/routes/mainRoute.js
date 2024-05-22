64cf9c2e1a1524e77033241a2e2a9c3bb1accf7d
Boinipally-Ayush <boinipallyayush@gmail.com>
2024-05-22 11:40:24 +0530

    course content saved

--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = require("./userAuth");
 const studentRouter = require("./studentCode");
+const questionRouter = require("./courseContent");
 const router = express.Router();
 
 router.use("/user", userRouter);
 router.use("/user", studentRouter);
+router.use("/user",questionRouter);
 module.exports = router;
