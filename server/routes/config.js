c2418ed6d61f347e868cb7a70faa46b0b92efbec
Md.Musab Haqqani <103328370+Musabhaqqani@users.noreply.github.com>
2024-08-07 16:36:56 +0530

    Update config.js

--- server/routes/config.js
+++ server/routes/config.js
@@ -1,5 +1,5 @@
 const JWT_SECRET = "Secret_Key"
-const url = "mongodb+srv://musabhaqqani:iUwJGslvNFSNjBOm@cluster0.onkpicf.mongodb.net/CodifyEd"
+const url = "Your mongodb uri"
 const localhost = "mongodb://0.0.0.0:27017/codifyLocal"
 
 module.exports = {JWT_SECRET, url, localhost}
